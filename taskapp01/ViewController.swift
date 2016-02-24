3e15895ffc2cb1cda222ae5a130347cf06e64fe0
株式会社コアファイン <corefine@kabushikikaishakoafain-no-macbook-pro.local>
2016-02-24 11:32:31 +0900

    データベースの準備が出来た。　『6.6 TableViewのプロトコルメソッドの中身を実装する』の直前まで。

--- taskapp01/ViewController.swift
+++ taskapp01/ViewController.swift
@@ -7,6 +7,34 @@
 //
 
 import UIKit
+import RealmSwift
+
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    // カテゴリ          ******* 課題対応
+    dynamic var category:String = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
@@ -20,6 +48,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
 
+    /***************  add by『6.5 Realm のデータベースファイル』*/
+    @IBOutlet weak var tableView: UITableView!
+     
+    // Realmインスタンスを取得する
+    let realm = try! Realm()  // ←追加
+    
+    // DB内のタスクが格納されるリスト。
+    // 日付近い順\順でソート：降順
+    // 以降内容をアップデートするとリスト内は自動的に更新される。
+    let taskArray = try! Realm().objects(Task).sorted("date",ascending: false)   // ←追加
+    /***************  add by『6.5 Realm のデータベースファイル』*/
+    
     
     // MARK: UITableViewDataSourceプロトコルのメソッド
     // データの数（＝セルの数）を返すメソッド
